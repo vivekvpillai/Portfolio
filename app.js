76b85a7ab9dbbe0f898fdd779156a5e1eaab23a4
Vijayan <vijayan@Vijayans-MBP.attlocal.net>
2021-12-13 11:00:34 -0600

    changes

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
-let slideIndex = 1;
+var slideIndex = 1;
 showSlides(slideIndex);
 
-let slides = document.getElementsByClassName("mySlides");
 
 // Next/previous controls
 function plusSlides(n) {
@@ -14,16 +13,12 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {
-    slideIndex = 1
-  }
-  if (n < 1) {
-    slideIndex = slides.length
-  }
-
+  console.log(slideIndex)
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
@@ -33,4 +28,3 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
-  slides[1].style.display = "active"
